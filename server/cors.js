338daf5585e6bca2a1bd72d0a51b693c81ed8c0d
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2023-05-26 05:04:26 +0530

    Delete server directory

--- server/cors.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-}
