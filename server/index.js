72b74b4d99f75532e33a778589ed06a8b91d5a72
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2023-06-01 01:47:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,16 +4,11 @@ const express = require('express')
 const path = require('path')
 const port = 5400
 const app  =  express()
-
-
 app.use(express.static(path.join(__dirname,"..","build")));
-
 app.use('/api',api)
-
-/*
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname,"..","build","index.html"));
-});*/
+});
 app.listen(port,()=>{
     console.log("Server up and running " + port);
 })
