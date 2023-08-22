c2cf65872deacb5ab4079653ef31d8b66d021e27
ManishMadan2882 <manishmadan321@gmail.com>
2023-08-23 04:52:27 +0530

    api updated

--- server/controllers/Profile/getUser.js
+++ server/controllers/Profile/getUser.js
@@ -2,7 +2,7 @@ const {user,blogs} = require('../../database/schema');
 const getUserById = async (req,res)=>{
   try{
    let searchedUser = await user.findOne({username:req.params.username})
-   .populate('blogs','title content createdAt');
+   .populate('blogs','title content createdAt imgUrl');
    
    if(searchedUser == null) 
    return res.status(404).json({msg:'user not found'})
