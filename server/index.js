e281752d0f66b29ab26d46b1ff0e57cb53b7df7f
ManishMadan2882 <manishmadan321@gmail.com>
2024-03-11 03:53:49 +0530

    modify build path

--- server/index.js
+++ server/index.js
@@ -5,9 +5,10 @@ const path = require('path')
 const port = 5400
 const app  =  express()
 app.use(express.static(path.join(__dirname,"..","build")));
-app.use('/api',api)
+app.use('/api',api) /*All the Routes are declared */
+/*Statically Hosting the frontend */
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,"..","build","index.html"));
+  res.sendFile(path.join(__dirname,"..","client","build","index.html"));
 });
 app.listen(port,()=>{
     console.log("Server up and running " + port);
