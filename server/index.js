acf1247dbaa221c8a3c454c7c7e56e3363dfa569
ManishMadan2882 <manishmadan321@gmail.com>
2024-03-11 04:15:24 +0530

    fix(static hosting): path of dir

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const express = require('express')
 const path = require('path')
 const port = 5400
 const app  =  express()
-app.use(express.static(path.join(__dirname,"..","build")));
+app.use(express.static(path.join(__dirname,"..","client","build")));
 app.use('/api',api) /*All the Routes are declared */
 /*Statically Hosting the frontend */
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,"..","client","build","index.html"));
+  res.sendFile(path.join(__dirname,"..","client","build"));
 });
 app.listen(port,()=>{
     console.log("Server up and running " + port);
