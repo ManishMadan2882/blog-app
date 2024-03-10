ef659d3f1fd9a1f91b7b442a70cb1cf25c33e32d
ManishMadan2882 <manishmadan321@gmail.com>
2024-03-11 01:59:27 +0530

    updating comment section

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,9 @@
-import React, { useEffect,useState } from 'react'
-import { Button} from '@mui/base';
+import React, { useEffect, useState } from 'react'
+import { Button } from '@mui/base';
 import EditDraft from './EditDraft';
-import { Edit} from '@mui/icons-material';
+import { Edit } from '@mui/icons-material';
 import Comment from './Comment';
-import { useParams,useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Cancel } from '@mui/icons-material';
 import { Delete } from '@mui/icons-material';
 import Modal from 'react-modal'
@@ -18,144 +18,135 @@ import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import { HashLoader } from 'react-spinners';
 
-const Blog = ({user}) => {
-  const {id} = useParams();
-  const navigate =  useNavigate();
-  const [commentModal,setCommentModalOpen ] = useState(false)
-  const [blog,setBlog] = useState([])
-  const [comments,setComments] = useState([])
-  const [openDel,setOpenDel] = useState(false)
-  const [openEdit,setOpenEdit] = useState(false)
-  const [loading,setLoading] = useState(true);
-  const deleteBlog = ()=>{ // makes DELETE request to remove the blog
+const Blog = ({ user }) => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [commentModal, setCommentModalOpen] = useState(false)
+  const [blog, setBlog] = useState([])
+  const [comments, setComments] = useState([])
+  const [openDel, setOpenDel] = useState(false)
+  const [openEdit, setOpenEdit] = useState(false)
+  const [loading, setLoading] = useState(true);
+  const deleteBlog = () => { // makes DELETE request to remove the blog
 
-    fetch(`/api/blog/${id}`,{
-      method : 'DELETE'
+    fetch(`/api/blog/${id}`, {
+      method: 'DELETE'
     }).then(res => res.json())
       .then(data => window.location.replace('/'))
-      .catch(err => console.log(err)) 
+      .catch(err => console.log(err))
 
   }
 
-  const callAPI = ()=> // loads the blog contents
+  const callAPI = () => // loads the blog contents
   {
-    
+
     fetch(`/api/blog/${id}`)
-    .then(res => res.json())
-    .then(data => {
-      if(data.msg === 'not found')
-      {
-        window.location.replace('/pagenotfound')
-      }
-      setBlog(data)
-    console.log(data);
-    setComments(data.comments)
-    setLoading(false)
-  })
+      .then(res => res.json())
+      .then(data => {
+        if (data.msg === 'not found') {
+          window.location.replace('/pagenotfound')
+        }
+        setBlog(data)
+        console.log(data);
+        setComments(data.comments)
+        setLoading(false)
+      })
     console.log(blog);
   }
-  useEffect(()=>{
+  useEffect(() => {
     callAPI()
-  },[])
+  }, [])
   return (
     <div>
-    <HashLoader
+      <HashLoader
         className='absolute mt-[50%] md:mt-[150px] mx-auto align-middle '
         color='#36d7b7'
         loading={loading}
         size={150}
         aria-label="Loading Spinner"
         data-testid="loader"
-    />
-     {!loading &&
-       <div>
-       {
+      />
+      {!loading &&
+        <div>
+          {
 
-(blog.account.username === user) && <div className='float-right p-2 ' >
-  <Button className='m-2'><Edit color='primary' onClick={()=>setOpenEdit(true)}/></Button>
-  <Modal isOpen={openEdit}>
-       <div >
-       <button onClick={()=>setOpenEdit(false)} className='float-right' >{<Cancel/>}</button>
-       </div>
-          <EditDraft title={blog.title} content={blog.content} imgUrl={blog.imgUrl} id={id}/>
-   </Modal>
- 
-  <Button className='m-2' title='Delete Blog' onClick={()=>setOpenDel(true)}> <Delete color='primary' fontSize='medium'/></Button>
-  <Dialog
-open={openDel}
-onClose={()=> setOpenDel(false)}
-aria-labelledby="alert-dialog-title"
-aria-describedby="alert-dialog-description"
->
-<DialogTitle id="alert-dialog-title">
-  {"Are you sure you want to delete this blog?"}
-</DialogTitle>
-<DialogContent>
-  <DialogContentText id="alert-dialog-description">
-    The Blog will be permanently deleted
-  </DialogContentText>
-</DialogContent>
-<DialogActions>
-  <Button  
-  className='p-2 m-2 bg-rose-800 text-white shadow-md'
-  onClick={()=>{
-    deleteBlog()
-    setOpenDel(false)
-    navigate(-1)
-  }} autoFocus>
-    Yes
-  </Button>
-  <Button  
-   className='p-2 m-2  bg-green-800 text-white shadow-md'
-  onClick={()=> setOpenDel(false)}>No</Button>
-</DialogActions>
-</Dialog>
-</div>}
-<div>
-   <h1 className='text-3xl p-3 text-center font-prata border-b-2 py-6'>
-       {blog.title}
-   </h1>          
+            (blog.account.username === user) && <div className='float-right p-2 ' >
+              <Button className='m-2'><Edit color='primary' onClick={() => setOpenEdit(true)} /></Button>
+              <Modal isOpen={openEdit}>
+                <div >
+                  <button onClick={() => setOpenEdit(false)} className='float-right' >{<Cancel />}</button>
+                </div>
+                <EditDraft title={blog.title} content={blog.content} imgUrl={blog.imgUrl} id={id} />
+              </Modal>
 
-   {blog.imgUrl &&  <div className='flex justify-center'>
-      <img alt="Image" className='py-6 max-w-full lg:max-w-[60%] drop-shadow-xl' src={blog.imgUrl}/>
-   </div>}
-     <div  className='flex justify-center '>
-       <div id='blogContent' className='shadow-lg overflow-hidden border h-max font-lora p-4 pb-28 lg:pb-14 leading-loose box-shadow-lg w-full lg:w-[60%] bg-white text-justify resize-none '>
-          
-        <ReactMarkdown  children={blog.content} rehypePlugins={[rehypeRaw]}/>     
-         
-     </div>
-     </div>
-    </div>
+              <Button className='m-2' title='Delete Blog' onClick={() => setOpenDel(true)}> <Delete color='primary' fontSize='medium' /></Button>
+              <Dialog
+                open={openDel}
+                onClose={() => setOpenDel(false)}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogTitle id="alert-dialog-title">
+                  {"Are you sure you want to delete this blog?"}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    The Blog will be permanently deleted
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button
+                    className='p-2 m-2 bg-rose-800 text-white shadow-md'
+                    onClick={() => {
+                      deleteBlog()
+                      setOpenDel(false)
+                      navigate(-1)
+                    }} autoFocus>
+                    Yes
+                  </Button>
+                  <Button
+                    className='p-2 m-2  bg-green-800 text-white shadow-md'
+                    onClick={() => setOpenDel(false)}>No</Button>
+                </DialogActions>
+              </Dialog>
+            </div>}
+          <div>
+            <h1 className='text-3xl p-3 text-center font-prata border-b-2 py-6'>
+              {blog.title}
+            </h1>
 
-    <div className='p-4 flex   justify-center'>
-    
-      <Button className=' w-full lg:w-[60%] border-gray-600 m-8 box-shadow-lg border py-2 ' onClick={() => setCommentModalOpen(true)}><AddCommentIcon fontSize='large' />{comments && comments.length}</Button>
-    </div>
-    {
-      <Modal className=' flex justify-center' onRequestClose={()=> setCommentModalOpen(false)}  isOpen={commentModal}>
-            <div className='mt-[20%] w-full md:w-auto bg-gradient-to-r from-cyan-700 to-cyan-600  p-4 '>
-               <button onClick={()=> setCommentModalOpen(false)} className='block float-right p-4' >{<Cancel/>}</button>
-            <NewComment setComments comments blogId={id} closeModal = {setCommentModalOpen} updateInfo = {callAPI} />
+            {blog.imgUrl && <div className='flex justify-center'>
+              <img alt="Image" className='py-6 max-w-full lg:max-w-[60%] drop-shadow-xl' src={blog.imgUrl} />
+            </div>}
+            <div className='flex justify-center '>
+              <div id='blogContent' className='shadow-lg overflow-hidden border h-max font-lora p-4 pb-28 lg:pb-14 leading-loose box-shadow-lg w-full lg:w-[60%] bg-white text-justify resize-none '>
+
+                <ReactMarkdown children={blog.content} rehypePlugins={[rehypeRaw]} />
+
+              </div>
             </div>
-       </Modal>  
-    }
-    <div className='flex justify-center'>
-    <div className='font-prata w-full lg:w-[60%] grid-flow-row grid-cols-1 '>
-      {
-        
-        comments && comments.map((element,key) =>{
-          return <Comment account={element.account} comment={element.comment}/>
-        })
-      }           
-      
-    </div>
-    </div>
-       </div>
+
+          <div className='p-2 flex  justify-center'>
+          <NewComment setComments comments blogId={id} updateInfo={callAPI} />
+          </div>
+          </div>
+
+          <div className='flex justify-center'>
+            <div className='font-prata w-full lg:w-[60%] grid-flow-row grid-cols-1 '>
+              {
+
+                comments && comments.map((element, key) => {
+                  return <Comment account={element.account} comment={element.comment} />
+                })
+              }
+
+            </div>
+          </div>
+        </div>
       }
-  
-    
-    
+
+
+
 
     </div>
   )
--- src/components/NewComment.js
+++ src/components/NewComment.js
@@ -1,50 +1,55 @@
 import React from 'react'
-import { SendOutlined} from '@mui/icons-material'
+import { SendOutlined } from '@mui/icons-material'
 import { TextareaAutosize } from '@mui/base'
 import { useState } from 'react'
 import { ClipLoader } from 'react-spinners'
-const NewComment = ({blogId,closeModal,updateInfo}) => {
-    const [text,setText] = useState('')
-    const [loading,setLoading] = useState(false)
-    function comment(){
-      setLoading(true);
-       fetch(`/api/comment/${blogId}`,{
-        method:"post",
-        headers: {
-            'Content-Type': 'application/json'
-          },
-        body:JSON.stringify({
-            comment : text
-        })
-       })
-       .then((res)=> res.json())
-       .then(data => {
+const NewComment = ({ blogId, updateInfo }) => {
+  const [text, setText] = useState('')
+  const [loading, setLoading] = useState(false)
+  function comment() {
+    setLoading(true);
+    fetch(`/api/comment/${blogId}`, {
+      method: "post",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        comment: text
+      })
+    })
+      .then((res) => res.json())
+      .then(data => {
         console.log(data.msg)
-        if(data.msg === 'saved')
-        {
+        if (data.msg === 'saved') {
           setLoading(false);
           updateInfo();
-          closeModal();
         }
         else window.location.replace('/login')
-        
-    }
-        
-       )
-       .catch(err => console.log(err))  
-    }
+
+      }
+
+      )
+      .catch(err => console.log(err))
+  }
   return (
-    <div >
-       <label className='block text-white'>Add Comment</label>
-       <TextareaAutosize value={text} onChange={(e)=> setText(e.target.value)} placeholder='Write the Comment here' className='bg-gray-100 p-2 w-full ' minRows={3}/>
-       <button onClick={()=> comment()} className="w-full px-4 py-2 bg-gray-200" >
-           {
+    <div className='w-full  lg:w-[60%]'>
+      <form className="mb-6">
+        <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border-2 border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+          <label for="comment" className="sr-only">Your comment</label>
+          <textarea value={text} onChange={(e) => setText(e.target.value)} id="comment" rows="6"
+            className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+            placeholder="Write a comment..." required></textarea>
+        </div>
+        <button onClick={comment} type="submit"
+          className="inline-flex items-center py-3 px-6 text-base font-medium text-center text-white bg-[#1d4ed8] rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-[#1e3a8a] hover:bg-[#1e40af]">
+          {
             loading ?
-            <ClipLoader color="#36d7b7" loading={loading} width={'100%'} />
-            :
-           <SendOutlined  color='secondary'/>
-           }
-       </button> 
+              <ClipLoader color="#36d7b7" loading={loading} width={'100%'} />
+              :
+              "Post"
+          }
+        </button>
+      </form>
     </div>
   )
 }
